5d0e03785e7a059b07e3cc82ad502ae9d266146d
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-26 15:06:27 +0100

    created api folder and foundation and views folder

---
+++ views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+@main.route('/add_movie', methods=['POST'])
+def add_movie():
+    return 'Done', 201
+
+@main.route('/movies')
+def movies():
+
+    movies=[]
+
+    return jsonify({'movies': movie})
